3b3e915dcb269812df6058427f7da8227194cb32
Francisco Vaz <ftomasvaz@gmail.com>
2020-11-21 09:53:25 +0000

    ngforof demo component

---
+++ src/app/components/ngforof-demo/ngforof-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngforof-demo',
+  templateUrl: './ngforof-demo.component.html',
+  styleUrls: ['./ngforof-demo.component.css']
+})
+export class NgforofDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
