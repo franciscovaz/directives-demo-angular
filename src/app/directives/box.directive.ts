450e76d1442423cd897df1190b57116deff558dd
Francisco Vaz <ftomasvaz@gmail.com>
2020-11-21 18:47:46 +0000

    create custom directive to change elements

--- src/app/directives/box.directive.ts
+++ src/app/directives/box.directive.ts
@@ -1,14 +1,23 @@
-import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBox]'
 })
 export class BoxDirective implements AfterViewInit {
 
+  @Input()
+  size = '1px';
+
+  @Input()
+  type = 'solid';
+
+  @Input()
+  color = 'red';
+
   constructor(private elem: ElementRef) { }
 
   ngAfterViewInit(): void {
-    this.elem.nativeElement.style.border = '1px solid black';
+    this.elem.nativeElement.style.border = `${this.size} ${this.type} ${this.color}`;
     this.elem.nativeElement.style.borderRadius = '5px';
     this.elem.nativeElement.style.padding = '5px';
     this.elem.nativeElement.style.margin = '5px';
