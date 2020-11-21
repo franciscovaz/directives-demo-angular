2743e969a2440191b39254e39abd616d563fbfee
Francisco Vaz <ftomasvaz@gmail.com>
2020-11-21 10:28:56 +0000

    show people from array with ngFor

--- src/app/components/ngforof-demo/ngforof-demo.component.ts
+++ src/app/components/ngforof-demo/ngforof-demo.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface People {
+  name: string;
+  age: number;
+  city: string;
+  picture: string;
+}
+
 @Component({
   selector: 'app-ngforof-demo',
   templateUrl: './ngforof-demo.component.html',
@@ -7,9 +15,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgforofDemoComponent implements OnInit {
 
+  people: Array<People>;
+
+  imgStyles = {
+    'height': '100px',
+    'width': '100px',
+    'border-radius': '12px',
+    'margin': '5px'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.people = [
+      { name: 'Francisco', age: 25, city: 'Gafanha de Aquém', picture:'/assets/images/francisco.jpg' },
+      { name: 'Vaz', age: 33, city: 'Gafanha de Aquém', picture:'/assets/images/capaTulha.jpg' },
+      { name: 'Logitech Name', age: 17, city: 'Aquém', picture:'/assets/images/nogeres.jpg' },
+      { name: 'Tomás', age: 52, city: 'Gafanha', picture:'/assets/images/francisco.jpg' },
+    ]
+  }
+
+  getCssClass(age: number) {
+    if(age >=18) {
+      return 'text-success'
+    } else {
+      return 'text-danger'
+    }
   }
 
 }
